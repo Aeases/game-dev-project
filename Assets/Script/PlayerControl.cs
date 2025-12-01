891cc22fb6e47e43a276886412b548c948f65f79
ColdMilo <93720350+ColdMilo@users.noreply.github.com>
2025-12-01 17:28:43 +0800

    started movement overhaul tests

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -32,6 +32,7 @@ public class PlayerControl : Shooter
     public static PlayerControl Instance;
     public int coin = 1000;
     public float maxHealth = 100f;
+    private Rigidbody rb;
     private Coroutine healthRegenOverTime;
     private void Awake()
     {
@@ -59,55 +60,11 @@ public class PlayerControl : Shooter
             return;
         }
         RotateToMouse();
-        PlayerMovementAndDash();
         HandleShooting();
         mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, transform.position.z); // Camera follows player (top-down)
     }
 
-    void PlayerMovementAndDash()
-    {
-        if (coolDownTimer > 0f) // Calculating CD
-        {
-            coolDownTimer -= Time.deltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && coolDownTimer <= 0)
-        {
-            Vector3 currentMovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-
-
-
-            if(currentMovementInput.x != 0 || currentMovementInput.z != 0) // Check is player is moving or not
-            {
-                dashDirection = currentMovementInput;  // If Y then dash to moving direction
-            } else
-            {
-                dashDirection = transform.forward; // If N then dash to where player is facing at 
-            }
-            isDashing = true;
-            dashTimer = dashDuration; 
-            coolDownTimer = dashCoolDown; // Reset the CD Timer
-        }
-        if (isDashing)
-        {
-            Vector3 dashMovement = dashDirection * dashPower * Time.deltaTime;
-            transform.Translate(dashMovement, Space.World);
-
-            dashTimer -= Time.deltaTime; // Duration of dashing
-            if (dashTimer <= 0f)
-            {
-                isDashing = false;
-            }
-        } else
-        {
-            // Move in world space (XZ plane)
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            moveInput = new Vector3(x, 0, z).normalized;
-            Vector3 move = moveInput * speed * Time.deltaTime;
-            transform.Translate(move, Space.World);
-        }
-    }
+    
     void HandleShooting()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
